fa6cb2036f71cc41cfe8e82f728b88aea8575d8f
Danillo Ilggner <danilloilggner@gmail.com>
2022-02-03 16:40:00 -0300

    #issue2 fixed

--- lib/cards/card.author.dart
+++ lib/cards/card.author.dart
@@ -19,37 +19,40 @@ class AuthorCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(20),
-      child: Row(
-        //TODO: add alignment
-        children: [
-          CircleImage(
-            imageProvider: imageProvider,
-            imageRadius: 30,
-          ),
-          Gap.gapW8,
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                authorName,
-                style: FooderlichTheme.light.textTheme.headline2,
-              ),
-              Text(
-                title,
-                style: FooderlichTheme.light.textTheme.headline3,
-              ),
-            ],
-          ),
-          IconButton(
-            icon: const Icon(Icons.favorite_border),
-            iconSize: 30,
-            color: Colors.grey[400],
-            onPressed: () {
-              const snackBar = SnackBar(content: Text('Favorite Pressed'));
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            },
-          ),
-        ],
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          //TODO: add alignment
+          children: [
+            CircleImage(
+              imageProvider: imageProvider,
+              imageRadius: 30,
+            ),
+            Gap.gapW8,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  authorName,
+                  style: FooderlichTheme.light.textTheme.headline2,
+                ),
+                Text(
+                  title,
+                  style: FooderlichTheme.light.textTheme.headline3,
+                ),
+              ],
+            ),
+            IconButton(
+              icon: const Icon(Icons.favorite_border),
+              iconSize: 30,
+              color: Colors.grey[400],
+              onPressed: () {
+                const snackBar = SnackBar(content: Text('Favorite Pressed'));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
