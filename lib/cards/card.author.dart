afac0b088969b88069152a143829aaf179e55245
Danillo Ilggner <danilloilggner@gmail.com>
2022-02-03 16:34:01 -0300

    card.author.dart improved

--- lib/cards/card.author.dart
+++ lib/cards/card.author.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/circle_image.dart';
+import 'package:fooderlich/theme.dart';
+import 'package:fooderlich/utils.dart';
 
 class AuthorCard extends StatelessWidget {
   const AuthorCard({
@@ -14,11 +17,39 @@ class AuthorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: replace Container
     return Container(
       padding: const EdgeInsets.all(20),
       child: Row(
-        children: [],
+        //TODO: add alignment
+        children: [
+          CircleImage(
+            imageProvider: imageProvider,
+            imageRadius: 30,
+          ),
+          Gap.gapW8,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                authorName,
+                style: FooderlichTheme.light.textTheme.headline2,
+              ),
+              Text(
+                title,
+                style: FooderlichTheme.light.textTheme.headline3,
+              ),
+            ],
+          ),
+          IconButton(
+            icon: const Icon(Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.grey[400],
+            onPressed: () {
+              const snackBar = SnackBar(content: Text('Favorite Pressed'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+          ),
+        ],
       ),
     );
   }
