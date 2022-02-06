ec08a101394f2a14632383beaba5edb50893369b
Danillo Ilggner <danilloilggner@gmail.com>
2022-02-06 17:20:27 -0300

    completed chapter 5 first challenge of flutter apprentice

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -4,10 +4,40 @@ import 'package:fooderlich/components/components.dart';
 import 'package:fooderlich/models/models.dart';
 import 'package:fooderlich/utils.dart';
 
-class ExplorePage extends StatelessWidget {
+class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
 
-  static final _mockService = MockFooderlichService();
+  @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  final _mockService = MockFooderlichService();
+  final _scrollController = ScrollController();
+
+  void _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('iaaiaiia fundo');
+    } else if (_scrollController.offset <=
+            _scrollController.position.minScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('uiuiuiuiu topo');
+    }
+  }
+
+  @override
+  void initState() {
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +46,7 @@ class ExplorePage extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: _scrollController,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
